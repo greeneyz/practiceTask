796a42cbc1dbb67b81292d12cfab392836fe3da3
Asim Imtiaz <Asim.ali.awan@gmail.com>
2016-10-06 09:01:49 -0400

    creating angular script and post script

---
+++ js/angular.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp', []);
+app.controller('customersCtrl', function($scope, $http)
+	{
+    $http.get("DAL.php")
+    .then(function (response) {$scope.guestbook = response.data;});
+});
